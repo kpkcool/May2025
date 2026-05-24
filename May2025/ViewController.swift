90dd31eb534a4ca6831c055625f485efdde044d3
K Praveen Kumar <kpkcool@gmail.com>
2026-05-24 19:39:25 +0530

    Rename Owner class to Person in ViewController

--- May2025/ViewController.swift
+++ May2025/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     }
 }
 
-class Owner {
+class Person {
     var name: String?
     var pet: Pet?
     
@@ -43,9 +43,9 @@ class Owner {
 }
 
 class Pet {
-    weak var owner: Owner?
+    weak var owner: Person?
     
-    init(owner: Owner? = nil) {
+    init(owner: Person? = nil) {
         self.owner = owner
         print("init Pet")
     }
