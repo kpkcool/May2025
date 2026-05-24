58b3260a10961a9250073ed3c6c3e54f4f6bfe35
K Praveen Kumar <kpkcool@gmail.com>
2026-05-24 19:37:39 +0530

    reference cycle demo

--- May2025/ViewController.swift
+++ May2025/ViewController.swift
@@ -8,15 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var owner1: Owner? = Owner()
+    var pet1: Pet? = Pet()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        owner1?.pet = pet1
+        pet1?.owner = owner1
+        
+        pet1 = nil
+        owner1 = nil
     }
-
 
     func sleep(){
         print("sleep Ankita")
+    }
+}
+
+class Owner {
+    var name: String?
+    var pet: Pet?
+    
+    init (pet: Pet? = nil) {
+        self.pet = pet
+        print("init Owner")
+    }
+    
+    deinit {
+        print("deinit Owner")
+    }
+    
+}
+
+class Pet {
+    weak var owner: Owner?
+    
+    init(owner: Owner? = nil) {
+        self.owner = owner
+        print("init Pet")
+    }
+    
+    deinit {
+        print("deinit Pet")
     }
 }
 
